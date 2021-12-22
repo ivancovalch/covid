6c6bd4dd7714d063d002370bbac72184f1a9cb76
ivancovalch <ivan7878@mail.ru>
2021-12-22 22:23:58 +0300

    create test directory

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from kivymd.app import MDApp
 from kivymd.uix.label import MDLabel
 
-
-
 class MainApp(MDApp):
     def build(self):
         return MDLabel(text = "Hello world", halign = "center")
-
-
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
