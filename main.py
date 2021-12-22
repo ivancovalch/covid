a512ecb88c872fe55faef282dd100aec5a8b5861
ivancovalch <ivan7878@mail.ru>
2021-12-22 18:18:13 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+
+
+class MainApp(MDApp):
+    def build(self):
+        return MDLabel(text = "Hello world", halign = "center")
+
+    
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    MainApp().run()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
