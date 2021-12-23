926ff633b60c5b3dbcff13f5b2ceb0edc1d269dd
ivancovalch <ivan7878@mail.ru>
2021-12-23 16:04:06 +0300

    down to kivy no md

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 
 from kivy.lang import Builder
-from kivymd.app import MDApp
+from kivy.app import App
 
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.list import IRightBodyTouch
-class Container(IRightBodyTouch, MDBoxLayout):
+from kivy.uix.boxlayout import BoxLayout
+
+
+class Container(BoxLayout):
     adaptive_width = True
 
-class App(MDApp):
+class App(App):
     def build(self):
         self.title = 'Setting Screen UI'
-        self.theme_cls.primary_palette = "Pink"
-        self.theme_cls.theme_style = "Dark"  # "Light"
+        #self.theme_cls.primary_palette = "Pink"
+        #self.theme_cls.theme_style = "Dark"  # "Light"
         return Builder.load_file('main.kv')
 
 
