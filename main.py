d7255297615ed19804277d1f529411239c18b1cd
ivancovalch <ivan7878@mail.ru>
2021-12-24 15:05:54 +0300

    1. py fike manager 2. spec require plyer

--- main.py
+++ main.py
@@ -7,33 +7,17 @@ from kivy.properties import ListProperty
 from kivy.uix.button import Button
 
 
-class FileChoose(Button):
-    '''
-    Button that triggers 'filechooser.open_file()' and processes
-    the data response from filechooser Activity.
-    '''
-
+class FileChoose(Button): # Button that triggers 'filechooser.open_file()' and processes the data response from filechooser Activity.
     selection = ListProperty([])
 
-    def choose(self):
-        '''
-        Call plyer filechooser API to run a filechooser Activity.
-        '''
+    def choose(self): # Call plyer filechooser API to run a filechooser Activity.
         filechooser.open_file(on_selection=self.handle_selection)
 
-    def handle_selection(self, selection):
-        '''
-        Callback function for handling the selection response from Activity.
-        '''
+    def handle_selection(self, selection): # Callback function for handling the selection response from Activity.
         self.selection = selection
 
-    def on_selection(self, *a, **k):
-        '''
-        Update TextInput.text after FileChoose.selection is changed
-        via FileChoose.handle_selection.
-        '''
+    def on_selection(self, *a, **k): #Update TextInput.text after FileChoose.selection is changed via FileChoose.handle_selection.
         App.get_running_app().root.ids.result.text = str(self.selection)
-
 
 class ChooserApp(App):
     def build(self):
