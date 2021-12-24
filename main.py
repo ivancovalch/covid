4c495eab600c3b9aee380d058c1d8ce865232f39
ivancovalch <ivan7878@mail.ru>
2021-12-24 14:41:37 +0300

    1. py fike manager 2. spec require plyer

--- main.py
+++ main.py
@@ -1,19 +1,44 @@
+from textwrap import dedent
 
+from plyer import filechooser
+from kivy.app import App
 from kivy.lang import Builder
-#from kivy.app import App
-from kivymd.app import MDApp
-from kivy.uix.boxlayout import BoxLayout
-from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.properties import ListProperty
+from kivy.uix.button import Button
 
-class Container(MDBoxLayout):
-    adaptive_width = True
 
-class MainApp(MDApp):
+class FileChoose(Button):
+    '''
+    Button that triggers 'filechooser.open_file()' and processes
+    the data response from filechooser Activity.
+    '''
+
+    selection = ListProperty([])
+
+    def choose(self):
+        '''
+        Call plyer filechooser API to run a filechooser Activity.
+        '''
+        filechooser.open_file(on_selection=self.handle_selection)
+
+    def handle_selection(self, selection):
+        '''
+        Callback function for handling the selection response from Activity.
+        '''
+        self.selection = selection
+
+    def on_selection(self, *a, **k):
+        '''
+        Update TextInput.text after FileChoose.selection is changed
+        via FileChoose.handle_selection.
+        '''
+        App.get_running_app().root.ids.result.text = str(self.selection)
+
+
+class ChooserApp(App):
     def build(self):
-        self.title = 'Setting Screen UI'
-        #self.theme_cls.primary_palette = "Pink"
-        #self.theme_cls.theme_style = "Dark"  # "Light"
         return Builder.load_file('main.kv')
 
 
-MainApp().run()
+if __name__ == '__main__':
+    ChooserApp().run()
